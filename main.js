8d2a1277ba713547930b6a302e06f8437cab933a
Emmaibe <emmanuelibenwankwo@gmail.com>
2023-04-15 14:58:49 +0100

    html, css and js

---
+++ main.js
@@ -0,0 +1,11 @@
+const iconHamburger = document.querySelector(".icon-hamburger");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  iconHamburger.toggleAttribute("data-invisible");
+});
